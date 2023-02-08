180b170b60c44e43d9c5d379b6033bb7807d4570
Angus Hsu <apangus611@gmail.com>
2023-02-08 17:12:11 +0800

    correct function name

--- app/routers/national_id.py
+++ app/routers/national_id.py
@@ -6,7 +6,7 @@ router = APIRouter(prefix='/national_id', tags=['national_id'])
 
 
 @router.post("/validate")
-async def test(payload: ValidationRequestPayload):
+async def validate(payload: ValidationRequestPayload):
     module = importlib.import_module(f'idnumbers.nationalid.{payload.country}')
     is_valid = module.NationalID.validate(payload.id_number)
     return ValidationResponse(is_valid=is_valid)
